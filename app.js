601f0add1462147f23db1301d2f0ad77f6d6e588
jaber19 <127965587+jaber19@users.noreply.github.com>
2025-01-20 16:26:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.use(express.json());
 const podName = process.env.POD_NAME;
 var ready = true;
 
+app.get('/greet', (req, res) => {
+  console.log(`Pod ${podName} is executing the greetings`);
+  res.send('Hello! Welcome to the Express app.');
+});
 
 app.post('/add', (req, res) => {
   const { num1, num2 } = req.body;
@@ -17,6 +21,14 @@ app.post('/add', (req, res) => {
   res.json({ result });
 });
 
+app.get('/error', (req, res) => {
+  console.log(`Pod ${podName} is executing the error process.exit.`);
+  process.exit(1);
+});
+
+app.get('/newgreet', (req, res) => {
+  res.send('Hello! This is the other greet route.');
+});
 
 app.get('/make-unready', (req, res) => {
   ready = false; // Simulate temporary unavailability
